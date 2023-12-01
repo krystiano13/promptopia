176a3fbce96bb559bed4ea2edb1527cae54c9a40
krystiano13 <crystianotv@gmail.com>
2023-12-01 22:00:30 +0100

    create home view

--- app/page.jsx
+++ app/page.jsx
@@ -6,8 +6,10 @@ const Home = () => {
                 <br className="max-md:hidden" />
                 <span className="orange_gradient text-center">AI-Powered prompts</span>
             </h1>
-            <p>
+            <p className="desc text-center">
                 Promptopia is an open-source AI prompting tool
+                for modern world to discover, create and share
+                creative prompts
             </p>
         </section>
     )
