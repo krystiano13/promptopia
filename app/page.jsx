aaea1bd8e963f7da227a6f2de5edc84f1d0e3009
krystiano13 <crystianotv@gmail.com>
2023-12-01 21:58:25 +0100

    create repo

---
+++ app/page.jsx
@@ -0,0 +1,16 @@
+const Home = () => {
+    return (
+        <section class="w-full flex-center flex-col">
+            <h1 className="head_text text-center">
+                Discover & Share
+                <br className="max-md:hidden" />
+                <span className="orange_gradient text-center">AI-Powered prompts</span>
+            </h1>
+            <p>
+                Promptopia is an open-source AI prompting tool
+            </p>
+        </section>
+    )
+}
+
+export default Home;
