c5f8984b8133880ed8eb3fbec3854baa97a6cf99
krystiano13 <crystianotv@gmail.com>
2023-12-03 20:36:11 +0100

    create util to connect to database

---
+++ utils/database.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+let isConnected = false;
+export const connectToDB = async () => {
+    mongoose.set("strictQuery", true);
+
+    if(isConnected) {
+        console.log("Connected");
+        return;
+    }
+
+    try {
+        await mongoose.connect(process.env.MONGODB_URI, {
+            dbName: "Share_Prompt",
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+
+        isConnected = true;
+        console.log("connected");
+    }
+    catch (error){
+        console.log(error);
+    }
+}
