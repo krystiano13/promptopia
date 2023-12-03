1eace28d37ba5e18a9037a94d53d42a7401b499c
krystiano13 <crystianotv@gmail.com>
2023-12-03 20:48:12 +0100

    add db data

--- utils/database.js
+++ utils/database.js
@@ -10,7 +10,7 @@ export const connectToDB = async () => {
 
     try {
         await mongoose.connect(process.env.MONGODB_URI, {
-            dbName: "Share_Prompt",
+            dbName: "Promptopia",
             useNewUrlParser: true,
             useUnifiedTopology: true
         })
